788f8822e32262c5467dadce90d28bdf4137c974
Elbert <78057370+ElbertParra@users.noreply.github.com>
2025-08-09 11:52:58 -0500

    Add Cypress test scaffolding

--- src/main/java/servicios/HuespedServiceImpl.java
+++ src/main/java/servicios/HuespedServiceImpl.java
@@ -2,31 +2,39 @@ package servicios;
 
 import modelo.dominio.Persona;
 
-public class HuespedServiceImpl implements PersonaInterface{
+import java.util.HashMap;
+import java.util.Map;
 
+public class HuespedServiceImpl implements PersonaInterface {
+
+    private final Map<Integer, Persona> personas = new HashMap<>();
 
     @Override
     public void registrarPersona(Persona persona) {
-
+        personas.put(persona.getId(), persona);
     }
 
     @Override
     public void eliminarPersona(int id) {
-
+        personas.remove(id);
     }
 
     @Override
     public Persona buscarPersona(int id) {
-        return null;
+        return personas.get(id);
     }
 
     @Override
     public void actualizarPersona(Persona persona) {
-
+        personas.put(persona.getId(), persona);
     }
 
     @Override
     public void listarPersonas() {
+        personas.values().forEach(System.out::println);
+    }
 
+    public int totalPersonas() {
+        return personas.size();
     }
 }
